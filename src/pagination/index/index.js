55dce60e382b9361fe94c33758830455d22f6c71
SenzawaG <rfx.msp@gmail.com>
2021-12-01 13:31:06 +0700

    disable long press pop up adjustment

--- src/pagination/index/index.js
+++ src/pagination/index/index.js
@@ -1,14 +1,12 @@
 function absorbEvent_(event) {
   var e = event || window.event;
-  e.preventDefault && e.preventDefault();
   e.stopPropagation && e.stopPropagation();
   e.cancelBubble = true;
-  e.returnValue = true;
+  e.returnValue = false;
   return false;
 }
 
 function preventLongPressMenu(node) {
-  node.ontouchstart = absorbEvent_;
   node.ontouchmove = absorbEvent_;
   node.ontouchend = absorbEvent_;
   node.ontouchcancel = absorbEvent_;
