6922130713d44bde89747921023f3a6de9908107
SenzawaG <rfx.msp@gmail.com>
2021-12-01 14:14:06 +0700

    LP PopUp Adj

--- src/pagination/index/index.js
+++ src/pagination/index/index.js
@@ -1,7 +1,6 @@
 //Prevent Pop Up on Long Press
 function absorbEvent_(event) {
   var e = event || window.event;
-  e.preventDefault && e.preventDefault();
   e.stopPropagation && e.stopPropagation();
   e.cancelBubble = true;
   e.returnValue = false;
@@ -9,7 +8,6 @@ function absorbEvent_(event) {
 }
 
 function preventLongPressMenu(node) {
-  node.ontouchstart = absorbEvent_;
   node.ontouchmove = absorbEvent_;
   node.ontouchend = absorbEvent_;
   node.ontouchcancel = absorbEvent_;
