b632ac14dfc3c8284cc6c92e48bb6ed9a0a9241e
SenzawaG <rfx.msp@gmail.com>
2021-12-01 14:15:14 +0700

    LP PopUp Removed

--- src/pagination/index/index.js
+++ src/pagination/index/index.js
@@ -1,25 +1,25 @@
 //Prevent Pop Up on Long Press
-function absorbEvent_(event) {
-  var e = event || window.event;
-  e.stopPropagation && e.stopPropagation();
-  e.cancelBubble = true;
-  e.returnValue = false;
-  return false;
-}
+// function absorbEvent_(event) {
+//   var e = event || window.event;
+//   e.stopPropagation && e.stopPropagation();
+//   e.cancelBubble = true;
+//   e.returnValue = false;
+//   return false;
+// }
 
-function preventLongPressMenu(node) {
-  node.ontouchmove = absorbEvent_;
-  node.ontouchend = absorbEvent_;
-  node.ontouchcancel = absorbEvent_;
-}
+// function preventLongPressMenu(node) {
+//   node.ontouchmove = absorbEvent_;
+//   node.ontouchend = absorbEvent_;
+//   node.ontouchcancel = absorbEvent_;
+// }
 
 
 
-let ch = document.getElementsByTagName('img');
-for (let i = 0; i < ch.length; i++) {
-  preventLongPressMenu(ch[i]);
-}
-console.log('sc')
+// let ch = document.getElementsByTagName('img');
+// for (let i = 0; i < ch.length; i++) {
+//   preventLongPressMenu(ch[i]);
+// }
+// console.log('sc')
 
 
 
