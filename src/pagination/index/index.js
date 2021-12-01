0fdb57382be33123232d076577c894c2c7ad740e
SenzawaG <rfx.msp@gmail.com>
2021-12-01 13:03:35 +0700

    disable long press pop up

--- src/pagination/index/index.js
+++ src/pagination/index/index.js
@@ -1,5 +1,19 @@
-window.oncontextmenu = function(event) {
-     event.preventDefault();
-     event.stopPropagation();
-     return false;
-};
+function absorbEvent_(event) {
+  var e = event || window.event;
+  e.preventDefault && e.preventDefault();
+  e.stopPropagation && e.stopPropagation();
+  e.cancelBubble = true;
+  e.returnValue = false;
+  return false;
+}
+
+function preventLongPressMenu(node) {
+  node.ontouchstart = absorbEvent_;
+  node.ontouchmove = absorbEvent_;
+  node.ontouchend = absorbEvent_;
+  node.ontouchcancel = absorbEvent_;
+}
+
+function init() {
+  preventLongPressMenu(document.getElementById('theimage'));
+}
