f7c79822516b37f44cacce773d7f616fd8bace7a
SenzawaG <rfx.msp@gmail.com>
2021-12-03 13:36:20 +0700

    View Adjustment

--- src/pagination/index/index.js
+++ src/pagination/index/index.js
@@ -65,53 +65,7 @@ c1CharLeft.onclick = function() {
 }
 
 
-let c2PubChatId = 0;
-const c2PubChat = document.getElementById('pub-chat');
-const c2PC_transform_desktop_1 = () => {
-  c2PubChat.style.transform = "translateY(-100%)";
-  c2PubChatId += 1;
-}
-const c2PC_transform_desktop_2 = () => {
-  c2PubChat.style.transform = "translateY(-100px)";
-  c2PubChatId -= 1;
-}
-const c2PC_transform_mobile_1 = () => {
-  c2PubChat.style.transform = "translateY(-100%)";
-  c2PubChatId += 1;
-}
-const c2PC_transform_mobile_2 = () => {
-  c2PubChat.style.transform = "translateY(-100px)";
-  c2PubChatId -= 1;
-}
 
 
 
 
-//Change img based on width
-if (window.innerWidth < 780) {
-  c2PubChat.src = "./src/imgs/channel/public-chat-mobile.png";
-  c2PubChat.onclick = function() {
-    if (c2PubChatId === 0) {
-      c2PC_transform_mobile_1();
-    } else {
-      c2PC_transform_mobile_2();
-    }
-  }
-} else if (window.innerWidth < 990) {
-  c2PubChat.src = "./src/imgs/channel/public-chat-tablet.png";
-  c2PubChat.onclick = function() {
-    if (c2PubChatId === 0) {
-      c2PC_transform_mobile_1();
-    } else {
-      c2PC_transform_mobile_2();
-    }
-  }
-} else {
-  c2PubChat.onclick = function() {
-    if (c2PubChatId === 0) {
-      c2PC_transform_desktop_1();
-    } else {
-      c2PC_transform_desktop_2();
-    }
-  }
-}
